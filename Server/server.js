77bb177947c395fb8472e300a54a33b397d05560
GhostlyTK <tokelojr@gmail.com>
2023-05-28 20:30:46 +0200

    API working

--- Server/server.js
+++ Server/server.js
@@ -1,29 +1,36 @@
 const express = require('express');
-const https = require('https');
 const request = require('request');
 const app = express();
+const port = 3000;
 
-const options = {
-  method: "GET",
-  url: "https://yahoo-weather5.p.rapidapi.com/weather",
-  qs: {
-    location: `${cityname}`,
-    format: "json",
-    u: `${metric}`,
-  },
-  headers: {
-    "X-RapidAPI-Key": "7e978dc152mshbfba3c9c73f0746p19d966jsn71a8411948a8",
-    "X-RapidAPI-Host": "yahoo-weather5.p.rapidapi.com",
-  },
-};
+
 
 app.get('/', (req, res) => {
-    const{ cityname, metric } = req.body;
-    options.qs.location = cityname
-    options.qs.u = metric
-  request(options, function (error, response, body) {
-    if (error) throw new Error(error);
-    res.send(body);
-  });
+    let cityname = req.query.cityname;
+    const api = {
+        method: 'GET',
+        url:`https://yahoo-weather5.p.rapidapi.com/weather`,
+        qs: {
+            format: 'json',
+            location: cityname,
+            u: 'c',
+        },
+        headers: {
+            'x-rapidapi-host': 'yahoo-weather5.p.rapidapi.com',
+            'x-rapidapi-key': '7e978dc152mshbfba3c9c73f0746p19d966jsn71a8411948a8'
+        }
+    }
+    request(api, (error, response, body) => {
+        if (error) {
+            res.send(error);
+        }
+        res.send(body);
+    })
+    
 })
 
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on https://localhost:${port}`)
+})
