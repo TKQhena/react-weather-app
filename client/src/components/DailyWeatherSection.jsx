8ed76444fdba46c165c3fa6b5f9f353638127d5b
AcaJones-786 <apeerbhai24@gmail.com>
2023-06-05 21:55:56 +0200

    Update DailyWeatherSection.jsx

--- client/src/components/DailyWeatherSection.jsx
+++ client/src/components/DailyWeatherSection.jsx
@@ -8,37 +8,37 @@ function DailyWeatherSection(){
         <section class="daily-weather-section">
             <div>
                 <div><span id="date">Date.now()</span></div>
-                <div><img class="card-img" src={sunnyImg} alt="sunny_img" /></div>
+                <div><img className="card-img" src={sunnyImg} alt="sunny_img" /></div>
                 <div><span id="coverage">yea yea yea</span></div>
                 <div><span id="temperature">temp</span></div>
             </div>
             <div>
                 <div><span id="date">Date.now()</span></div>
-                <div><img class="card-img" src={sunnyImg} alt="sunny_img" /></div>
+                <div><img className="card-img" src={sunnyImg} alt="sunny_img" /></div>
                 <div><span id="coverage">yea yea yea</span></div>
                 <div><span id="temperature">temp</span></div>
             </div>
             <div>
                 <div><span id="date">Date.now()</span></div>
-                <div><img class="card-img" src={sunnyImg} alt="sunny_img" /></div>
+                <div><img className="card-img" src={sunnyImg} alt="sunny_img" /></div>
                 <div><span id="coverage">yea yea yea</span></div>
                 <div><span id="temperature">temp</span></div>
             </div>
             <div>
                 <div><span id="date">Date.now()</span></div>
-                <div><img class="card-img" src={sunnyImg} alt="sunny_img" /></div>
+                <div><img className="card-img" src={sunnyImg} alt="sunny_img" /></div>
                 <div><span id="coverage">yea yea yea</span></div>
                 <div><span id="temperature">temp</span></div>
             </div>
             <div>
                 <div><span id="date">Date.now()</span></div>
-                <div><img class="card-img" src={sunnyImg} alt="sunny_img" /></div>
+                <div><img className="card-img" src={sunnyImg} alt="sunny_img" /></div>
                 <div><span id="coverage">yea yea yea</span></div>
                 <div><span id="temperature">temp</span></div>
             </div>
             <div>
                 <div><span id="date">Date.now()</span></div>
-                <div><img class="card-img" src={sunnyImg} alt="sunny_img" /></div>
+                <div><img className="card-img" src={sunnyImg} alt="sunny_img" /></div>
                 <div><span id="coverage">yea yea yea</span></div>
                 <div><span id="temperature">temp</span></div>
             </div>
